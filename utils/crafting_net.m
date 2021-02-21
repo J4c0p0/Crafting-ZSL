e10e303e72f0a77f610d77c95f12b8aed427ca45
J4c0p0 <39941720+J4c0p0@users.noreply.github.com>
2021-02-21 10:43:30 +0100

    Update crafting_net.m

--- utils/crafting_net.m
+++ utils/crafting_net.m
@@ -1,3 +1,32 @@
-function crafted_net = crafting_net(net,dataset_name_,flag_semantic_craft)
+function crafted_layers = crafting_net(net,dataset_name_,flag_semantic_craft)
 
-path_ = pather();
+if flag_semantic_craft
+    if ~exist('./splits/class_embeddings_Xian.mat','file')
+        addpath('./splits')
+        write_class_embeddings(dataset_name_)
+    else
+        load('./splits/class_embeddings_Xian.mat','class_embeddings')
+        if ~isfield(class_embeddings,(dataset_name_))
+            addpath('./splits')
+            write_class_embeddings(dataset_name_)
+            load('./splits/class_embeddings_Xian.mat','class_embeddings')
+        end
+    end
+    
+    att = class_embeddings.(dataset_name_).data';
+    
+    crafted_layers = [net;
+    reluLayer;
+    dropoutLayer(0.5);
+    fullyConnectedLayer(size(att,1),'BiasL2Factor',20,'WeightL2Factor',10);
+    reluLayer;
+    fullyConnectedLayer(size(att,2),'BiasL2Factor',20,'WeightL2Factor',10);
+    softmaxLayer;
+    classificationLayer];
+
+    crafted_layers(end-2).Bias = zeros(size(att,2),1);
+    crafted_layers(end-2).Weights = att';
+    crafted_layers(end-2).BiasLearnRateFactor = 1e-7;
+    crafted_layers(end-2).WeightLearnRateFactor = 1e-7;
+        
+end
