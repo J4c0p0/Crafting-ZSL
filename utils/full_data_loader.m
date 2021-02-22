b233f630282bc810439a4d74f802c7b1499fa2dc
J4c0p0 <39941720+J4c0p0@users.noreply.github.com>
2021-02-22 06:51:46 +0100

    Update full_data_loader.m

--- utils/full_data_loader.m
+++ utils/full_data_loader.m
@@ -1,23 +1,38 @@
-function [imdbTrain, imdbTestUnseen, imdbTestSeen] =  full_data_loader(dataset_name_,UnseenClasses,TrainFileNames)
-%% Exemplar input to run
-%clear; close all;
-%dataset_name_ = 'FLO';
-%UnseenClasses = importdata(['../splits/' dataset_name_ '_unseen_classes.txt']);
-%splits = importdata(['../splits/' dataset_name_ '_splits.xls']);
-%TrainFileNames = splits.train_seen(:,2);
-%clear splits;
+function [imdbTrain, imdbTestUnseen, imdbTestSeen, UnseenClasses] =  full_data_loader(dataset_name_,flag_ZSL)
 
-paths = pather();
+path_ = pather();
 
-if isempty(TrainFileNames) %We're in standard, inductive ZSL
-    imdb = imageDatastore(paths.(dataset_name_),'IncludeSubfolders',true,...
+if ~exist(path_.(dataset_name_),'dir') && sprintf(dataset_name_,'AWA2')
+    addpath('./splits/')
+    path_ = emergency_AWA_2_downloader();
+else
+    error('Please specify the path for the current dataset, inside pather.m')
+end
+
+if exist(['./splits/' dataset_name_ '_unseen_classes.txt'],'file')
+    UnseenClasses = importdata(['../splits/' dataset_name_ '_unseen_classes.txt']);
+else
+    if exist(['./splits/create_txt_files_data_' dataset_name_ '.m'],'file')
+        run(['./splits/create_txt_files_data_' dataset_name_ '.m']);
+        UnseenClasses = importdata(['../splits/' dataset_name_ '_unseen_classes.txt']);
+    else
+        error('xls and txt files for (G)ZSL splits are needed!');
+    end
+end
+
+if flag_ZSL
+    imdb = imageDatastore(path_.(dataset_name_),'IncludeSubfolders',true,...
         'LabelSource','foldernames');
     [imdbTrain, imdbTestUnseen] = ZSL_data_loader(imdb,UnseenClasses);
     imdbTestSeen = [];
 else
-    imdb = imageDatastore(paths.(dataset_name_),'IncludeSubfolders',true,...
+    imdb = imageDatastore(path_.(dataset_name_),'IncludeSubfolders',true,...
         'LabelSource','foldernames');
     [imdbAllSeen, imdbTestUnseen] = ZSL_data_loader(imdb,UnseenClasses);
+    
+    splits = importdata(['./splits/' dataset_name_ '_splits.xls']);
+    TrainFileNames = splits.train_seen(:,2);
+    clear splits;
+    
     [imdbTrain,imdbTestSeen] = generalizedZSL_data_loader(imdbAllSeen,TrainFileNames);
 end
-
