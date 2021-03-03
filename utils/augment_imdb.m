0c5c06b45560af535e092360e3c8b8ebe6376fe8
J4c0p0 <39941720+J4c0p0@users.noreply.github.com>
2021-03-03 05:01:09 +0100

    Update augment_imdb.m

--- utils/augment_imdb.m
+++ utils/augment_imdb.m
@@ -1,4 +1,4 @@
-function [augimdb, Labels] = augment_imdb(imdb,size_)
+function [augimdb, Labels] = augment_imdb(imdb,size_,dataset_name_)
 
 Labels = imdb.Labels;
 augimdb = augmentedImageDatastore(size_,imdb,'ColorPreprocessing','gray2rgb');
