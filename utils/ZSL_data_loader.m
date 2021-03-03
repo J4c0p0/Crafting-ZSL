d9d805596044e312748276748ca041a57a8a8dcd
J4c0p0 <39941720+J4c0p0@users.noreply.github.com>
2021-03-03 05:01:41 +0100

    Update ZSL_data_loader.m

--- utils/ZSL_data_loader.m
+++ utils/ZSL_data_loader.m
@@ -1,8 +1,4 @@
 function [imdbSeen, imdbUnseen] = ZSL_data_loader(imdb,UnseenClasses)
-%% Exemplar input to run
-%clear; close all; clc;
-%imdb = imageDatastore('J:\ZSL_datasets\FLO','IncludeSubfolders',true,'LabelSource','foldernames');    
-%UnseenClasses = importdata('./double_check_data_loading/FLO_unseen_classes.txt');
 
 complete_path_to_images = imdb.Files;
 whr_ = contains(complete_path_to_images,UnseenClasses);
